196ca259f5c0d44e545edf29ba59a7c755dc4787
Jagrut Sharma <jagsharm@adobe.com>
2024-02-18 09:14:24 -0800

    ch 3 copy reference

--- Chapter03/copy_reference.cc
+++ Chapter03/copy_reference.cc
@@ -3,9 +3,9 @@
 #include <iostream>
 
 template <std::size_t N>
-void print_array_values(
-    const std::array<std::uint32_t, N> &arr) // input only: const, 8bytes
+void print_array_values(const std::array<std::uint32_t, N> &arr) // input only: const, 8bytes
 {
+    std::cout << "(in function by ref) Address of my_array is " << &arr << std::endl;
     for (std::size_t i = 0; i < arr.size(); i++)
     {
         std::cout << arr[i] << '\n';
@@ -13,8 +13,18 @@ void print_array_values(
 }
 
 template <std::size_t N>
-void double_value(
-    std::array<std::uint32_t, N> &arr) // in and output, no const, but reference
+void print_array_values_making_copy(const std::array<std::uint32_t, N> arr) // will make copy
+{
+    std::cout << "(in function by copy) Address of my_array is " << &arr << std::endl;
+    for (std::size_t i = 0; i < arr.size(); i++)
+    {
+        std::cout << arr[i] << '\n';
+    }
+}
+
+// The parameter arr is coming by reference, and will be modified by the function
+template <std::size_t N>
+void double_value(std::array<std::uint32_t, N> &arr) // in and output, no const, but reference
 {
     for (std::size_t i = 0; i < arr.size(); i++)
     {
@@ -25,7 +35,9 @@ void double_value(
 int main()
 {
     auto my_array = std::array<std::uint32_t, 5U>{1, 2, 3, 4, 5};
+    std::cout << "(main) Address of my_array is " << &my_array << std::endl;
     print_array_values(my_array);
+    print_array_values_making_copy(my_array);
     double_value(my_array);
     print_array_values(my_array);
 
