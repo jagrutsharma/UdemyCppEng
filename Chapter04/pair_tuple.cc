41ee4a034cbf8bf131080aaef4aad3f5a5e135dc
Jagrut Sharma <jagrutsharma@gmail.com>
2024-02-20 21:41:15 -0800

    ch 4 pair and tuple

--- Chapter04/pair_tuple.cc
+++ Chapter04/pair_tuple.cc
@@ -25,27 +25,34 @@ DataBlaBla some_functions2(const std::int32_t input)
 
 int main()
 {
+    std::cout << "=== Example 1 === " << std::endl;
     auto my_pair = std::pair<std::int32_t, float>{1337, 42.0F};
     std::cout << my_pair.first << '\n';
     std::cout << my_pair.second << '\n';
 
+    std::cout << "=== Example 2 === " << std::endl;
     auto my_tuple =
         std::tuple<std::int32_t, float, std::string>{1337, 42.0F, "Jan"};
     std::cout << std::get<0>(my_tuple) << '\n';
     std::cout << std::get<1>(my_tuple) << '\n';
     std::cout << std::get<2>(my_tuple) << '\n';
 
+    std::cout << "=== Example 3 === " << std::endl;
     const auto result = some_functions(42);
     std::cout << std::get<0>(result) << '\n';
     std::cout << std::get<1>(result) << '\n';
     std::cout << std::get<2>(result) << '\n';
 
-    const auto &[i, s, f] = some_functions(42); // Strucutred Binding
+    std::cout << "=== Example 4 === " << std::endl;
+    const auto &[i, s, f] = some_functions(42); // Strucutred Binding - This reminds me of destructuring in Javascript
     std::cout << i << '\n';
     std::cout << s << '\n';
     std::cout << f << '\n';
 
+    std::cout << "=== Example 5 === " << std::endl;
     const auto &[i1, f1] = some_functions2(42);
+    std::cout << i1 << '\n';
+    std::cout << f1 << '\n';
 
     return 0;
 }
