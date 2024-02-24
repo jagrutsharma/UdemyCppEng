4ca3767c951456aab15b2fe6a5cb4af90af67915
Jagrut Sharma <jagrutsharma@gmail.com>
2024-02-24 07:19:57 -0800

    ch 4 map

--- Chapter04/map.cc
+++ Chapter04/map.cc
@@ -2,8 +2,13 @@
 #include <iostream>
 #include <map>
 
+// Get as a reference, otherwise it will make a copy that we don't want
 void print_map(const std::map<std::string, std::int32_t> &map)
 {
+    // Iterating amap in a loop
+    // Get as a refernce using &
+    // Other if you use [key, val], it will make a copy which we likely don't want
+    // since we are just reading
     for (const auto &[key, val] : map)
     {
         std::cout << key << ": " << val << '\n';
@@ -14,16 +19,24 @@ void print_map(const std::map<std::string, std::int32_t> &map)
 
 int main()
 {
+    // key & value
     auto my_map = std::map<std::string, std::int32_t>{{"Jan", 28}};
 
     my_map["Sam"] = 40;
     my_map["Veronika"] = 24;
+    my_map["Tom"] = 23;
 
     print_map(my_map);
 
     my_map["Veronika"] = 25;
 
     print_map(my_map);
+
+    bool maryExists = my_map.contains("Mary");
+    std::cout << "Mary exists? " << maryExists << std::endl;
+    if (!maryExists) {
+        my_map["Mary"] = 72;
+    }
 
     if (!my_map.contains("Sam"))
     {
@@ -39,8 +52,9 @@ int main()
     const auto it_find = my_map.find("Lisa");
     if (it_find != my_map.end())
     {
+        // (*it_find) is the pair from map. Use the "first" to get the key.
         std::cout << (*it_find).first << '\n';
     }
 
-    return 0;
-}
+    std::cout << my_map["Lisa"] << std::endl;
+};
