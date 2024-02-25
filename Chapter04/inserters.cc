bd0a91de4fa18e50b007db1130a85e04998ec648
Jagrut Sharma <jagrutsharma@gmail.com>
2024-02-24 20:54:53 -0800

    ch 4 inserters

--- Chapter04/inserters.cc
+++ Chapter04/inserters.cc
@@ -9,6 +9,8 @@ int main()
     auto my_result2 = std::vector<int>{11, 12};
 
     // container must implement "insert"
+    // We are copying my_vector [0,1,2,3,4,5] into my_result1 which already has [-1, -2]
+    // 2nd argument of std::inserter specifies the position index where we want to insert
     std::copy(my_vector.begin(),
               my_vector.end(),
               std::inserter(my_result1, std::next(my_result1.begin())));
@@ -18,6 +20,7 @@ int main()
     my_result1.clear();
 
     // container must implement "push_back"
+    // std::back_inserter will always insert at the end
     std::copy(my_vector.begin(),
               my_vector.end(),
               std::back_inserter(my_result2));
