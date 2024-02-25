dc5388510a1803d0b2fdf7406195687063a1cd40
Jagrut Sharma <jagrutsharma@gmail.com>
2024-02-25 00:11:06 -0800

    ch 4 exercises on iterators

--- Chapter04/Exercise/Exercise/IteratorLib.h
+++ Chapter04/Exercise/Exercise/IteratorLib.h
@@ -8,4 +8,40 @@ namespace mystd
 using It = std::vector<std::int32_t>::iterator;
 using DifferenceType = It::difference_type;
 
+void advance(It &it, DifferenceType n) {
+    // std::advance(it, n);
+    if (n > 0) {
+        while (n > 0) {
+            it++;
+            n--;
+        }
+    }
+    else {
+        while (n < 0) {
+            it--;
+            n++;
+        }
+    }
+}
+
+DifferenceType distance(It last, It first) {
+    // return std::distance(last, first);
+    auto result = DifferenceType{0};
+    while (first != last) {
+        first++;
+        result++;
+    }
+    return result;
+}
+
+It next(It it, DifferenceType n = 1) {
+    std::advance(it, n);
+    return it;
+}
+
+It prev(It it, DifferenceType n = 1) {
+    std::advance(it, -n);
+    return it;
+}
+
 } // namespace mystd
