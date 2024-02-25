b98c33bcc44229bf4087482ca3dbc999b10ec306
Jagrut Sharma <jagrutsharma@gmail.com>
2024-02-24 19:19:49 -0800

    ch 4 templates

--- Chapter04/templates.cc
+++ Chapter04/templates.cc
@@ -3,12 +3,28 @@
 #include <iostream>
 #include <span>
 #include <vector>
+using namespace std;
+// All standard library containers are templated classes
+// template type (C++) = generic type (Java)
 
 template <typename T>
 void print_container(std::span<T> span)
 {
+    cout << "Templatized Container" << endl;
     for (const auto val : span)
     {
+        std::cout << val << '\n';
+    }
+}
+
+template <typename X> X add_nums2(X a, X b) {
+    return a + b;
+}
+
+// This function only takes int32_t type. Better to use generic type.
+void print_container2(std::span<std::int32_t> span) {
+    cout << "Normal Container" << endl;
+    for (const auto val : span) {
         std::cout << val << '\n';
     }
 }
@@ -22,6 +38,8 @@ int main()
     print_container<std::int32_t>(my_vec);
     print_container<std::uint16_t>(my_arr);
     print_container<std::uint64_t>(my_c_arr);
+
+    cout << add_nums2(32, 1) << endl;
 
     return 0;
 }
